635986945564732aed43b520bc9a05ec10a3d757
Rima <rsingh142005@gmail.com>
2026-07-04 23:49:46 +0530

    host update

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def predict():
 
     return render_template(
         'index.html',
-        prediction_text=result
-    )
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        prediction_text=result)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000, debug=True)
